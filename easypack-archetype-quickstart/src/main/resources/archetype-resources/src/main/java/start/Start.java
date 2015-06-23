d322d5546e4813e1183fa2764cab1e759248192f
Agustin Muñoz <amunoz@despegar.com>
2015-06-23 14:51:52 -0300

    QuickStart Archetype

---
+++ easypack-archetype-quickstart/src/main/resources/archetype-resources/src/main/java/start/Start.java
@@ -0,0 +1,14 @@
+package ${package}.start;
+
+/**
+ * EasyPack hello world!
+ * 
+ */
+public class Start {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+
+	}
+}
